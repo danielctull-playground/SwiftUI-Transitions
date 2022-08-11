59117ea5044edc47063a11af092f4469a801d813
Daniel Tull <dt@danieltull.co.uk>
2022-08-11 11:21:13 +0100

    Initial Commit

---
+++ SwiftUI Transitions/SwiftUI_TransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TransitionsApp.swift
+//  SwiftUI Transitions
+//
+//  Created by Daniel Tull on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
